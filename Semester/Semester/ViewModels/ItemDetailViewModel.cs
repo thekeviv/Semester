3d2b300219c5c381b4ef2913e67f870e838af818
Vivek Sharma <thekeviv.vivek@gmail.com>
2019-05-08 18:40:09 -0700

    Add project files.

---
+++ Semester/Semester/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Semester.Models;
+
+namespace Semester.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}

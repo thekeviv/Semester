e4c0f4e3029d85745cdee7016148c01d841d494c
Vivek Sharma <thekeviv.vivek@gmail.com>
2019-05-10 17:02:50 -0700

    Added Tasks models

---
+++ Semester/Semester/Models/Tasks/CourseTasks.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+namespace Semester.Models
+{
+    public class CourseTasks:Tasks 
+    {
+        public Guid CourseId { get; set; }
+    }
+}
---
+++ Semester/Semester/Models/Tasks/Tasks.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+namespace Semester.Models
+{
+    public class Tasks
+    {
+        public Tasks()
+        {
+            this.Id = Guid.NewGuid();
+        }
+        [PrimaryKey]
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public DateTime DueDate { get; set; }
+        public Boolean isDone { get; set; }
+    }
+}
